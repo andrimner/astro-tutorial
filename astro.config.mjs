830915e5f169ce8469a4d3f7c1f76b9d9e499102
Tommy <tommy.lindstrom@shiverlabs.ai>
2024-08-19 15:36:44 +0200

    Added dark mode and dynamic greeting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nimble-yeot-1dc5df.netlify.app/"
+  site: "https://nimble-yeot-1dc5df.netlify.app/",
+  integrations: [preact()]
 });
